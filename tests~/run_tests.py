76a63d03755f7986129569bec71e14fc093bbb38
Francis M. Tyers <ftyers@prompsit.com>
2008-05-08 16:43:48 +0000

    Removing old tests
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/lttoolbox@4777 72bbbca6-d526-0410-a7d9-f06f51895060

--- tests~/run_tests.py
+++
@@ -1,7 +0,0 @@
-import unittest
-import null_flush
-
-if __name__ == "__main__":
-    suite = unittest.TestLoader().loadTestsFromModule(null_flush)
-    unittest.TextTestRunner(verbosity = 2).run(suite)
-
