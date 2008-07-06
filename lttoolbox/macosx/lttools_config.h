edf464da53c59658d429a5746a58c971c4ddd031
Sergio Ortiz Rojas <sergio.ortiz@gmail.com>
2008-07-06 16:39:38 +0000

    New files for Mac OS X
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/lttoolbox@5653 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ lttoolbox/macosx/lttools_config.h
@@ -0,0 +1,123 @@
+/* lttoolbox/lttools_config.h.  Generated from lttools_config.h.in by configure.  */
+/* lttoolbox/lttools_config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the declaration of `fgetc_unlocked', and to 0 if
+   you don't. */
+#define HAVE_DECL_FGETC_UNLOCKED 0
+
+/* Define to 1 if you have the declaration of `fputs_unlocked', and to 0 if
+   you don't. */
+#define HAVE_DECL_FPUTS_UNLOCKED 0
+
+/* Define to 1 if you have the declaration of `fputwc_unlocked', and to 0 if
+   you don't. */
+#define HAVE_DECL_FPUTWC_UNLOCKED 0
+
+/* Define to 1 if you have the declaration of `fputws_unlocked', and to 0 if
+   you don't. */
+#define HAVE_DECL_FPUTWS_UNLOCKED 0
+
+/* Define to 1 if you have the declaration of `fread_unlocked', and to 0 if
+   you don't. */
+#define HAVE_DECL_FREAD_UNLOCKED 0
+
+/* Define to 1 if you have the declaration of `fwrite_unlocked', and to 0 if
+   you don't. */
+#define HAVE_DECL_FWRITE_UNLOCKED 0
+
+/* Define to 1 if you have the declaration of ` fgetwc_unlocked', and to 0 if
+   you don't. */
+#define HAVE_DECL___________________FGETWC_UNLOCKED 0
+
+/* Define to 1 if you have the declaration of `\ fputc_unlocked', and to 0 if
+   you don't. */
+#define HAVE_DECL____________________FPUTC_UNLOCKED 0
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the `getopt_long' function. */
+#define HAVE_GETOPT_LONG 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `xml2' library (-lxml2). */
+#define HAVE_LIBXML2 1
+
+/* Define to 1 if your system has a GNU libc compatible `malloc' function, and
+   to 0 otherwise. */
+#define HAVE_MALLOC 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if your system has a GNU libc compatible `realloc' function,
+   and to 0 otherwise. */
+#define HAVE_REALLOC 1
+
+/* Define to 1 if you have the `setlocale' function. */
+#define HAVE_SETLOCALE 1
+
+/* Define to 1 if stdbool.h conforms to C99. */
+#define HAVE_STDBOOL_H 1
+
+/* Define to 1 if you have the <stddef.h> header file. */
+#define HAVE_STDDEF_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the `strdup' function. */
+#define HAVE_STRDUP 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if the system has the type `_Bool'. */
+/* #undef HAVE__BOOL */
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "sortiz@users.sourceforge.net"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "lttoolbox/lttoolbox.h"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "lttoolbox/lttoolbox.h 3.0.4"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "lttoolbox-lttoolbox-h"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "3.0.4"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define to rpl_malloc if the replacement function should be used. */
+/* #undef malloc */
+
+/* Define to rpl_realloc if the replacement function should be used. */
+/* #undef realloc */
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
