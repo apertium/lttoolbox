db59b6564f0a13a8c1a18d6d0224c77242fbf119
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2014-02-05 07:47:33 +0000

    tests: cd to testdir before running so user can do simply '$ tests/run_tests.py'
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/lttoolbox@49997 72bbbca6-d526-0410-a7d9-f06f51895060

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python2
 
 import sys
-import os.path as path
-sys.path.append(path.realpath("."))
+import os
+sys.path.append(os.path.realpath("."))
 
 import unittest
 import lt_proc
 
 if __name__ == "__main__":
+    os.chdir(os.path.dirname(__file__))
     suite = unittest.TestLoader().loadTestsFromModule(lt_proc)
     unittest.TextTestRunner(verbosity = 2).run(suite)
