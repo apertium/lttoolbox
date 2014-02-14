b515554a6282a42c04ba3133c4b9105eb29c2bf0
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2014-02-14 09:24:05 +0000

    lt-trim: a test for empty; constants for +/<compound-only-L>/<compound-R>
    + tests refactored a bit
    
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/lttoolbox@50233 72bbbca6-d526-0410-a7d9-f06f51895060

--- tests/lt_trim/__init__.py
+++ tests/lt_trim/__init__.py
@@ -9,36 +9,36 @@ from shutil import rmtree
 from proctest import ProcTest
 from subprocess import Popen, PIPE
 
-class TrimNormalAndJoin(unittest.TestCase, ProcTest):
-    inputs = ["abc", "ab", "y", "n",
-              "jg", "jh", "kg"
-    ]
-    expectedOutputs = ["^abc/ab<n><def>$", "^ab/ab<n><ind>$", "^y/y<n><ind>$", "^n/*n$",
-                       "^jg/j<pr>+g<n>$", "^jh/*jh$", "^kg/*kg$"
-    ]
-    expectedRetCode = 0
+class TrimProcTest(ProcTest):
+    monodix = "data/minimal-mono.dix"
+    monodir = "lr"
+    bidix = "data/minimal-bi.dix"
+    bidir = "lr"
+    procflags = ["-z"]
 
     def runTest(self):
         tmpd = mkdtemp()
         try:
             self.assertEqual(0, call(["../lttoolbox/lt-comp",
-                                      "lr",
-                                      "data/minimal-mono.dix",
-                                      tmpd+"/minimal-mono.bin"],
+                                      self.monodir,
+                                      self.monodix,
+                                      tmpd+"/mono.bin"],
                                      stdout=PIPE))
             self.assertEqual(0, call(["../lttoolbox/lt-comp",
-                                      "lr",
-                                      "data/minimal-bi.dix",
-                                      tmpd+"/minimal-bi.bin"],
+                                      self.bidir,
+                                      self.bidix,
+                                      tmpd+"/bi.bin"],
                                      stdout=PIPE))
             self.assertEqual(0, call(["../lttoolbox/lt-trim",
-                                      tmpd+"/minimal-mono.bin",
-                                      tmpd+"/minimal-bi.bin",
-                                      tmpd+"/minimal-trimmed.bin"],
+                                      tmpd+"/mono.bin",
+                                      tmpd+"/bi.bin",
+                                      tmpd+"/trimmed.bin"],
                                      stdout=PIPE))
 
-            self.cmdLine = ["../lttoolbox/.libs/lt-proc", "-z", tmpd+"/minimal-trimmed.bin"]
-            self.proc = Popen(self.cmdLine, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+            self.proc = Popen(["../lttoolbox/lt-proc"] + self.procflags + [tmpd+"/trimmed.bin"],
+                              stdin=PIPE,
+                              stdout=PIPE,
+                              stderr=PIPE)
 
             for inp,exp in zip(self.inputs, self.expectedOutputs):
                 self.assertEqual( self.communicateFlush(inp+"[][\n]"),
@@ -56,178 +56,63 @@ class TrimNormalAndJoin(unittest.TestCase, ProcTest):
             rmtree(tmpd)
 
 
+class TrimNormalAndJoin(unittest.TestCase, TrimProcTest):
+    inputs = ["abc", "ab", "y", "n", "jg", "jh", "kg"]
+    expectedOutputs = ["^abc/ab<n><def>$", "^ab/ab<n><ind>$", "^y/y<n><ind>$", "^n/*n$", "^jg/j<pr>+g<n>$", "^jh/*jh$", "^kg/*kg$"]
+    expectedRetCode = 0
 
-
-class TrimCmp(unittest.TestCase, ProcTest):
+class TrimCmp(unittest.TestCase, TrimProcTest):
     inputs = ["a", "b", "c", "d", "aa", "ab", "ac", "ad", "ba", "bb", "bc", "bd", "ca", "cb", "cc", "cd", "da", "db", "dc", "dd", ]
     expectedOutputs = ["^a/*a$", "^b/b<n>$", "^c/*c$", "^d/d<n>$", "^aa/*aa$", "^ab/a<n>+b<n>$", "^ac/*ac$", "^ad/a<n>+d<n>$", "^ba/*ba$", "^bb/*bb$", "^bc/*bc$", "^bd/*bd$", "^ca/*ca$", "^cb/d<n>+b<n>$", "^cc/*cc$", "^cd/d<n>+d<n>$", "^da/*da$", "^db/*db$", "^dc/*dc$", "^dd/*dd$"]
     expectedRetCode = 0
+    monodix = "data/cmp-mono.dix"
+    bidix = "data/cmp-bi.dix"
+    procflags = ["-e", "-z"]
 
-    def runTest(self):
-        tmpd = mkdtemp()
-        try:
-            self.assertEqual(0, call(["../lttoolbox/lt-comp",
-                                      "lr",
-                                      "data/cmp-mono.dix",
-                                      tmpd+"/cmp-mono.bin"],
-                                     stdout=PIPE))
-            self.assertEqual(0, call(["../lttoolbox/lt-comp",
-                                      "lr",
-                                      "data/cmp-bi.dix",
-                                      tmpd+"/cmp-bi.bin"],
-                                     stdout=PIPE))
-            self.assertEqual(0, call(["../lttoolbox/lt-trim",
-                                      tmpd+"/cmp-mono.bin",
-                                      tmpd+"/cmp-bi.bin",
-                                      tmpd+"/cmp-trimmed.bin"],
-                                     stdout=PIPE))
-
-            self.cmdLine = ["../lttoolbox/.libs/lt-proc", "-e", "-z", tmpd+"/cmp-trimmed.bin"]
-            self.proc = Popen(self.cmdLine, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-
-            for inp,exp in zip(self.inputs, self.expectedOutputs):
-                self.assertEqual( self.communicateFlush(inp+"[][\n]"),
-                                  exp+"[][\n]" )
-
-            self.proc.communicate() # let it terminate
-            self.proc.stdin.close()
-            self.proc.stdout.close()
-            self.proc.stderr.close()
-            self.assertEqual( self.proc.poll(),
-                              self.expectedRetCode )
-
-
-        finally:
-            rmtree(tmpd)
-
-
-
-
-
-class TrimLongleft(unittest.TestCase, ProcTest):
+class TrimLongleft(unittest.TestCase, TrimProcTest):
     inputs = ["herdende"]
     expectedOutputs = ["^herdende/herde<adj><pprs>$"]
     expectedRetCode = 0
+    monodix = "data/longleft-mono.dix"
+    bidix = "data/longleft-bi.dix"
 
-    def runTest(self):
-        tmpd = mkdtemp()
-        try:
-            self.assertEqual(0, call(["../lttoolbox/lt-comp",
-                                      "lr",
-                                      "data/longleft-mono.dix",
-                                      tmpd+"/longleft-mono.bin"],
-                                     stdout=PIPE))
-            self.assertEqual(0, call(["../lttoolbox/lt-comp",
-                                      "lr",
-                                      "data/longleft-bi.dix",
-                                      tmpd+"/longleft-bi.bin"],
-                                     stdout=PIPE))
-            self.assertEqual(0, call(["../lttoolbox/lt-trim",
-                                      tmpd+"/longleft-mono.bin",
-                                      tmpd+"/longleft-bi.bin",
-                                      tmpd+"/longleft-trimmed.bin"],
-                                     stdout=PIPE))
-
-            self.cmdLine = ["../lttoolbox/.libs/lt-proc", "-e", "-z", tmpd+"/longleft-trimmed.bin"]
-            self.proc = Popen(self.cmdLine, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-
-            for inp,exp in zip(self.inputs, self.expectedOutputs):
-                self.assertEqual( self.communicateFlush(inp+"[][\n]"),
-                                  exp+"[][\n]" )
-
-            self.proc.communicate() # let it terminate
-            self.proc.stdin.close()
-            self.proc.stdout.close()
-            self.proc.stderr.close()
-            self.assertEqual( self.proc.poll(),
-                              self.expectedRetCode )
-
-
-        finally:
-            rmtree(tmpd)
-
-class BidixPardef(unittest.TestCase, ProcTest):
+class BidixPardef(unittest.TestCase, TrimProcTest):
     inputs = ["c"]
     expectedOutputs = ["^c/c<vblex><inf>$"]
     expectedRetCode = 0
+    monodix = "data/bidixpardef-mono.dix"
+    bidir = "rl"
+    bidix = "data/bidixpardef-bi.dix"
 
-    def runTest(self):
-        tmpd = mkdtemp()
-        try:
-            self.assertEqual(0, call(["../lttoolbox/lt-comp",
-                                      "lr",
-                                      "data/bidixpardef-mono.dix",
-                                      tmpd+"/bidixpardef-mono.bin"],
-                                     stdout=PIPE))
-            self.assertEqual(0, call(["../lttoolbox/lt-comp",
-                                      "rl", # rl!
-                                      "data/bidixpardef-bi.dix",
-                                      tmpd+"/bidixpardef-bi.bin"],
-                                     stdout=PIPE))
-            self.assertEqual(0, call(["../lttoolbox/lt-trim",
-                                      tmpd+"/bidixpardef-mono.bin",
-                                      tmpd+"/bidixpardef-bi.bin",
-                                      tmpd+"/bidixpardef-trimmed.bin"],
-                                     stdout=PIPE))
-
-            self.cmdLine = ["../lttoolbox/.libs/lt-proc", "-z", tmpd+"/bidixpardef-trimmed.bin"]
-            self.proc = Popen(self.cmdLine, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-
-            for inp,exp in zip(self.inputs, self.expectedOutputs):
-                self.assertEqual( self.communicateFlush(inp+"[][\n]"),
-                                  exp+"[][\n]" )
-
-            self.proc.communicate() # let it terminate
-            self.proc.stdin.close()
-            self.proc.stdout.close()
-            self.proc.stderr.close()
-            self.assertEqual( self.proc.poll(),
-                              self.expectedRetCode )
-
-
-        finally:
-            rmtree(tmpd)
-
-
-
-class UnbalancedEpsilons(unittest.TestCase, ProcTest):
+class UnbalancedEpsilons(unittest.TestCase, TrimProcTest):
     inputs = ["re", "rer", "res", "ret"]
     expectedOutputs = ["^re/re<vblex><inf>$", "^rer/re<vblex><pres>$", "^res/re<vblex><pres>$", "^ret/re<vblex><pret>$"]
     expectedRetCode = 0
+    monodix = "data/unbalanced-epsilons-mono.dix"
+    bidir = "rl"
+    bidix = "data/unbalanced-epsilons-bi.dix"
 
+
+class Empty(unittest.TestCase, TrimProcTest):
     def runTest(self):
         tmpd = mkdtemp()
         try:
             self.assertEqual(0, call(["../lttoolbox/lt-comp",
                                       "lr",
-                                      "data/unbalanced-epsilons-mono.dix",
-                                      tmpd+"/unbalanced-epsilons-mono.bin"],
+                                      "data/empty-mono.dix",
+                                      tmpd+"/empty-mono.bin"],
                                      stdout=PIPE))
             self.assertEqual(0, call(["../lttoolbox/lt-comp",
                                       "rl", # rl!
-                                      "data/unbalanced-epsilons-bi.dix",
-                                      tmpd+"/unbalanced-epsilons-bi.bin"],
+                                      "data/empty-bi.dix",
+                                      tmpd+"/empty-bi.bin"],
                                      stdout=PIPE))
-            self.assertEqual(0, call(["../lttoolbox/lt-trim",
-                                      tmpd+"/unbalanced-epsilons-mono.bin",
-                                      tmpd+"/unbalanced-epsilons-bi.bin",
-                                      tmpd+"/unbalanced-epsilons-trimmed.bin"],
-                                     stdout=PIPE))
-
-            self.cmdLine = ["../lttoolbox/.libs/lt-proc", "-z", tmpd+"/unbalanced-epsilons-trimmed.bin"]
-            self.proc = Popen(self.cmdLine, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-
-            for inp,exp in zip(self.inputs, self.expectedOutputs):
-                self.assertEqual( self.communicateFlush(inp+"[][\n]"),
-                                  exp+"[][\n]" )
-
-            self.proc.communicate() # let it terminate
-            self.proc.stdin.close()
-            self.proc.stdout.close()
-            self.proc.stderr.close()
-            self.assertEqual( self.proc.poll(),
-                              self.expectedRetCode )
-
+            self.assertEqual(1, call(["../lttoolbox/lt-trim",
+                                      tmpd+"/empty-mono.bin",
+                                      tmpd+"/empty-bi.bin",
+                                      tmpd+"/empty-trimmed.bin"],
+                                     stdout=PIPE,
+                                     stderr=PIPE))
 
         finally:
             rmtree(tmpd)
