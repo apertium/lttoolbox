1492390e5f27ee189f52d3b7c26f03d71d20ebe5
unhammer <unhammer@72bbbca6-d526-0410-a7d9-f06f51895060>
2014-02-14 09:23:17 +0000

    lt-trim: handle compound tags too
    We simply skip over them by setting trimmer_trg to trimmer_src.
    More testing TODO, also accept the non-standard compound tags
    and use constants from fst_processor.{cc,h}
    
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/lttoolbox@50228 72bbbca6-d526-0410-a7d9-f06f51895060

--- tests/lt_trim/__init__.py
+++ tests/lt_trim/__init__.py
@@ -9,7 +9,7 @@ from shutil import rmtree
 from proctest import ProcTest
 from subprocess import Popen, PIPE
 
-class Trim(unittest.TestCase, ProcTest):
+class TrimNormalAndJoin(unittest.TestCase, ProcTest):
     inputs = ["abc", "ab", "y", "n",
               "jg", "jh", "kg"
     ]
@@ -38,6 +38,51 @@ class Trim(unittest.TestCase, ProcTest):
                                      stdout=PIPE))
 
             self.cmdLine = ["../lttoolbox/.libs/lt-proc", "-z", tmpd+"/minimal-trimmed.bin"]
+            self.proc = Popen(self.cmdLine, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+
+            for inp,exp in zip(self.inputs, self.expectedOutputs):
+                self.assertEqual( self.communicateFlush(inp+"[][\n]"),
+                                  exp+"[][\n]" )
+
+            self.proc.communicate() # let it terminate
+            self.proc.stdin.close()
+            self.proc.stdout.close()
+            self.proc.stderr.close()
+            self.assertEqual( self.proc.poll(),
+                              self.expectedRetCode )
+
+
+        finally:
+            rmtree(tmpd)
+
+
+
+
+class TrimCmp(unittest.TestCase, ProcTest):
+    inputs = ["a", "b", "c", "d", "aa", "ab", "ac", "ad", "ba", "bb", "bc", "bd", "ca", "cb", "cc", "cd", "da", "db", "dc", "dd", ]
+    expectedOutputs = ["^a/*a$", "^b/b<n>$", "^c/*c$", "^d/d<n>$", "^aa/*aa$", "^ab/a<n>+b<n>$", "^ac/*ac$", "^ad/a<n>+d<n>$", "^ba/*ba$", "^bb/*bb$", "^bc/*bc$", "^bd/*bd$", "^ca/*ca$", "^cb/d<n>+b<n>$", "^cc/*cc$", "^cd/d<n>+d<n>$", "^da/*da$", "^db/*db$", "^dc/*dc$", "^dd/*dd$"]
+    expectedRetCode = 0
+
+    def runTest(self):
+        tmpd = mkdtemp()
+        try:
+            self.assertEqual(0, call(["../lttoolbox/lt-comp",
+                                      "lr",
+                                      "data/cmp-mono.dix",
+                                      tmpd+"/cmp-mono.bin"],
+                                     stdout=PIPE))
+            self.assertEqual(0, call(["../lttoolbox/lt-comp",
+                                      "lr",
+                                      "data/cmp-bi.dix",
+                                      tmpd+"/cmp-bi.bin"],
+                                     stdout=PIPE))
+            self.assertEqual(0, call(["../lttoolbox/lt-trim",
+                                      tmpd+"/cmp-mono.bin",
+                                      tmpd+"/cmp-bi.bin",
+                                      tmpd+"/cmp-trimmed.bin"],
+                                     stdout=PIPE))
+
+            self.cmdLine = ["../lttoolbox/.libs/lt-proc", "-e", "-z", tmpd+"/cmp-trimmed.bin"]
             self.proc = Popen(self.cmdLine, stdin=PIPE, stdout=PIPE, stderr=PIPE)
 
             for inp,exp in zip(self.inputs, self.expectedOutputs):
