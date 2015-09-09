8a31646047b54078af1016d0545160f964a825ac
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2015-09-09 08:36:09 +0000

    exit with the number of failures
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/lttoolbox@61820 72bbbca6-d526-0410-a7d9-f06f51895060

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -9,6 +9,9 @@ import lt_proc, lt_trim
 
 if __name__ == "__main__":
     os.chdir(os.path.dirname(__file__))
+    failures = 0
     for module in [lt_trim, lt_proc]:
         suite = unittest.TestLoader().loadTestsFromModule(module)
-        unittest.TextTestRunner(verbosity = 2).run(suite)
+        res = unittest.TextTestRunner(verbosity = 2).run(suite)
+        failures += len(res.failures)
+    sys.exit(min(failures, 255))
