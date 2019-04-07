apertium/lttoolbox

commit 76829d02e148989844047c70a3d668d654ae4e97

Put spaces in blankqueue too, popping if we write a word with spaces
and only print *one* item from blankqueue in printSpace.

This fixes #47. With the input 'legge opp<br/>blah', we'll now have
the regular space in blankqueue and use that before getting to the
<br/>.

Tested on 100.000 lines of html with nno-nob, only good changes found,
and seems to work fine with flushing.

lttoolbox/fst_processor.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005 Universitat d'Alacant / Universidad de Alicante
+ * Copyright (C) 2005-2019 Universitat d'Alacant / Universidad de Alicante
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -284,6 +284,9 @@ FSTProcessor::readAnalysis(FILE *input)
       default:
         streamError();
     }
+  }
+  if(val == L' ') {
+    blankqueue.push(L" ");
   }
 
   input_buffer.add(val);
@@ -631,7 +634,7 @@ FSTProcessor::readBilingual(FILE *input, FILE *output)
 void
 FSTProcessor::flushBlanks(FILE *output)
 {
-  for(unsigned int i = blankqueue.size(); i > 0; i--)
+  for(size_t i = blankqueue.size(); i > 0; i--)
   {
     fputws_unlocked(blankqueue.front().c_str(), output);
     blankqueue.pop();
@@ -714,6 +717,22 @@ FSTProcessor::writeEscaped(wstring const &str, FILE *output)
 }
 
 void
+FSTProcessor::writeEscapedPopBlanks(wstring const &str, FILE *output)
+{
+  for(unsigned int i = 0, limit = str.size(); i < limit; i++)
+  {
+    if(escaped_chars.find(str[i]) != escaped_chars.end())
+    {
+      fputwc_unlocked(L'\\', output);
+    }
+    fputwc_unlocked(str[i], output);
+    if (str[i] == L' ' && blankqueue.front() == L" ") {
+      blankqueue.pop();
+    }
+  }
+}
+
+void
 FSTProcessor::writeEscapedWithTags(wstring const &str, FILE *output)
 {
   for(unsigned int i = 0, limit = str.size(); i < limit; i++)
@@ -739,6 +758,15 @@ FSTProcessor::printWord(wstring const &sf, wstring const &lf, FILE *output)
 {
   fputwc_unlocked(L'^', output);
   writeEscaped(sf, output);
+  fputws_unlocked(lf.c_str(), output);
+  fputwc_unlocked(L'$', output);
+}
+
+void
+FSTProcessor::printWordPopBlank(wstring const &sf, wstring const &lf, FILE *output)
+{
+  fputwc_unlocked(L'^', output);
+  writeEscapedPopBlanks(sf, output);
   fputws_unlocked(lf.c_str(), output);
   fputwc_unlocked(L'$', output);
 }
@@ -782,7 +810,8 @@ FSTProcessor::printSpace(wchar_t const val, FILE *output)
 {
   if(blankqueue.size() > 0)
   {
-    flushBlanks(output);
+    fputws_unlocked(blankqueue.front().c_str(), output);
+    blankqueue.pop();
   }
   else
   {
@@ -1409,8 +1438,8 @@ FSTProcessor::analysis(FILE *input, FILE *output)
       }
       else if(last_postblank)
       {
-        printWord(sf.substr(0, sf.size()-input_buffer.diffPrevPos(last)),
-                  lf, output);
+        printWordPopBlank(sf.substr(0, sf.size()-input_buffer.diffPrevPos(last)),
+                          lf, output);
         fputwc_unlocked(L' ', output);
         input_buffer.setPos(last);
         input_buffer.back(1);
@@ -1418,15 +1447,15 @@ FSTProcessor::analysis(FILE *input, FILE *output)
       else if(last_preblank)
       {
         fputwc_unlocked(L' ', output);
-        printWord(sf.substr(0, sf.size()-input_buffer.diffPrevPos(last)),
-                  lf, output);
+        printWordPopBlank(sf.substr(0, sf.size()-input_buffer.diffPrevPos(last)),
+                          lf, output);
         input_buffer.setPos(last);
         input_buffer.back(1);
       }
       else if(last_incond)
       {
-        printWord(sf.substr(0, sf.size()-input_buffer.diffPrevPos(last)),
-                  lf, output);
+        printWordPopBlank(sf.substr(0, sf.size()-input_buffer.diffPrevPos(last)),
+                          lf, output);
         input_buffer.setPos(last);
         input_buffer.back(1);
       }
@@ -1519,8 +1548,8 @@ FSTProcessor::analysis(FILE *input, FILE *output)
       }
       else
       {
-        printWord(sf.substr(0, sf.size()-input_buffer.diffPrevPos(last)),
-                  lf, output);
+        printWordPopBlank(sf.substr(0, sf.size()-input_buffer.diffPrevPos(last)),
+                          lf, output);
         input_buffer.setPos(last);
         input_buffer.back(1);
       }

lttoolbox/fst_processor.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005 Universitat d'Alacant / Universidad de Alicante
+ * Copyright (C) 2005-2019 Universitat d'Alacant / Universidad de Alicante
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -267,14 +267,14 @@ private:
   bool isEscaped(wchar_t const c) const;
 
   /**
-   * Read text from stream (analysis version, also used in postgeneration)
+   * Read text from stream (analysis version)
    * @param input the stream to read
    * @return the next symbol in the stream
    */
   int readAnalysis(FILE *input);
 
   /**
-   * Read text from stream (generation version, also used in generation)
+   * Read text from stream (decomposition version)
    * @param input the stream to read
    * @return the next symbol in the stream
    */
@@ -331,6 +331,14 @@ private:
    */
   void writeEscaped(wstring const &str, FILE *output);
 
+  /**
+   * Write a string to an output stream.
+   * If we print a space, we may pop a space from blankqueue.
+   *
+   * @param str the string to write, escaping characters
+   * @param output the stream to write in
+   */
+  void writeEscapedPopBlanks(wstring const &str, FILE *output);
 
   /**
    * Write a string to an output stream, escaping all escapable characters
@@ -356,6 +364,16 @@ private:
    * @param output stream where the word is written
    */
   void printWord(wstring const &sf, wstring const &lf, FILE *output);
+
+  /**
+   * Prints a word.
+   * If we print a space, we may pop a space from blankqueue.
+   *
+   * @param sf surface form of the word
+   * @param lf lexical form of the word
+   * @param output stream where the word is written
+   */
+  void printWordPopBlank(wstring const &sf, wstring const &lf, FILE *output);
 
   /**
    * Prints a word (Bilingual version)
@@ -386,7 +404,15 @@ private:
   int readTMAnalysis(FILE *input);
 
   unsigned int lastBlank(wstring const &str);
+
+  /**
+   * Print one blankqueue item if there is one, or a given "space" value.
+   *
+   * @param val the space character to use if no blank queue
+   * @param output stream where the word is written
+   */
   void printSpace(wchar_t const val, FILE *output);
+
   void skipUntil(FILE *input, FILE *output, wint_t const character);
   static wstring removeTags(wstring const &str);
   wstring compoundAnalysis(wstring str, bool uppercase, bool firstupper);

tests/data/gardenpath-mwe.dix
@@ -1,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<dictionary>
+  <alphabet>ABCDEFGHIJKLMNOPQRSTUVWXYZÀÁÂÄÅÆÇÈÉÊËÍÑÒÓÔÕÖØÙÚÜČĐŊŠŦŽabcdefghijklmnopqrstuvwxyzàáâäåæçèéêëíñòóôõöøùúüčđŋšŧž­-</alphabet>
+  <sdefs>
+    <sdef n="vblex"/>
+    <sdef n="inf"/>
+    <sdef n="pr"/>
+    <sdef n="prn"/>
+  </sdefs>
+  <pardefs>
+  </pardefs>
+
+  <section id="main" type="standard">
+    <e><p><l>legge</l> <r>legge<s n="vblex"/><s n="inf"/></r></p></e>
+    <e><p><l>legge<b/>opp<b/>til</l> <r>legge<g><b/>opp<b/>til<s n="vblex"/><s n="inf"/></g></r></p></e>
+    <e><p><l>opp</l> <r>opp<s n="pr"/></r></p></e>
+
+    <e><p><l>legge<b/>seg<b/>opp<b/>til</l> <r>legge<g><b/>seg<b/>opp<b/>til<s n="vblex"/><s n="inf"/></g></r></p></e>
+    <e><p><l>seg</l> <r>seg<s n="prn"/></r></p></e>
+  </section>
+
+</dictionary>

tests/lt_proc/__init__.py
@@ -92,5 +92,22 @@ class Intergeneration(unittest.TestCase, ProcTest):
     inputs = ["la dona ~dóna tot"]
     expectedOutputs = ["la dona dona tot"]
 
+class GardenPathMwe(unittest.TestCase, ProcTest):
+    procdix = "data/gardenpath-mwe.dix"
+    inputs          = ["x[ <br/> ]opp.",
+                        "legge opp[<br/>]x.",
+                       "y[A]x",
+                        "[A]y x",
+                        "legge opp[<br/>]",
+                        "legge[][\n]L[<\/p>\n]",
+                       ]
+    expectedOutputs = ["^x/*x$[ <br/> ]^opp/opp<pr>$.",
+                        "^legge/legge<vblex><inf>$ ^opp/opp<pr>$[<br/>]^x/*x$.",
+                       "^y/*y$[A]^x/*x$",
+                        "[A]^y/*y$ ^x/*x$",
+                        "^legge/legge<vblex><inf>$ ^opp/opp<pr>$[<br/>]",
+                        "^legge/legge<vblex><inf>$[][\n]^L/*L$[<\/p>\n]",
+                       ]
+
 # These fail on some systems:
 #from null_flush_invalid_stream_format import *

.gitignore
@@ -1,5 +1,6 @@
 *.pyc
-**.deps/
+**/*.deps/
+/.ccls-cache/
 /*.pc
 /aclocal.m4
 /autom4te.cache
@@ -57,3 +58,16 @@
 *.out
 *.app
 
+/compile_commands.json
+/lttoolbox/lt-comp
+/lttoolbox/lt-proc
+/lttoolbox/lt-trim
+/lttoolbox/Makefile
+/lttoolbox/Makefile.in
+/lttoolbox/lt-tmxcomp
+/lttoolbox/lt-print
+/lttoolbox/stamp-h1
+/lttoolbox/lttoolbox_config.h.in
+/lttoolbox/lttoolbox_config.h
+/lttoolbox/lt-tmxproc
+/lttoolbox/lt-expand

tests/proctest.py
@@ -66,6 +66,8 @@ class ProcTest():
                               stdout=PIPE,
                               stderr=PIPE)
 
+            self.assertEqual(len(self.inputs),
+                             len(self.expectedOutputs))
             for inp, exp in zip(self.inputs, self.expectedOutputs):
                 self.assertEqual(self.communicateFlush(inp+"[][\n]"),
                                  exp+"[][\n]")

commit 38d22d4232481350ec4b7fba45c6acda5956c458

Always print blanks of a word immediately after printing analysis
Fixes minor regression in 76829d02e148989844047c70a3d668d654ae4e97

E.g. if

St.[
]Petersburg

gets analysis

^St. Petersburg/St. Petersburg<np><top>/St. Petersburg<np><top>$

we should print the [\n] right after that:

^St. Petersburg/St. Petersburg<np><top>/St. Petersburg<np><top>$[
]

otherwise blanks in queue get shifted

lttoolbox/fst_processor.cc
@@ -716,20 +716,25 @@ FSTProcessor::writeEscaped(wstring const &str, FILE *output)
   }
 }
 
-void
+size_t
 FSTProcessor::writeEscapedPopBlanks(wstring const &str, FILE *output)
 {
-  for(unsigned int i = 0, limit = str.size(); i < limit; i++)
+  size_t postpop = 0;
+  for (unsigned int i = 0, limit = str.size(); i < limit; i++)
   {
-    if(escaped_chars.find(str[i]) != escaped_chars.end())
-    {
+    if (escaped_chars.find(str[i]) != escaped_chars.end()) {
       fputwc_unlocked(L'\\', output);
     }
     fputwc_unlocked(str[i], output);
-    if (str[i] == L' ' && blankqueue.front() == L" ") {
-      blankqueue.pop();
+    if (str[i] == L' ') {
+      if (blankqueue.front() == L" ") {
+        blankqueue.pop();
+      } else {
+        postpop++;
+      }
     }
   }
+  return postpop;
 }
 
 void
@@ -766,9 +771,14 @@ void
 FSTProcessor::printWordPopBlank(wstring const &sf, wstring const &lf, FILE *output)
 {
   fputwc_unlocked(L'^', output);
-  writeEscapedPopBlanks(sf, output);
+  size_t postpop = writeEscapedPopBlanks(sf, output);
   fputws_unlocked(lf.c_str(), output);
   fputwc_unlocked(L'$', output);
+  while (postpop-- && blankqueue.size() > 0)
+  {
+    fputws(blankqueue.front().c_str(), output);
+    blankqueue.pop();
+  }
 }
 
 void

lttoolbox/fst_processor.h
@@ -333,12 +333,14 @@ private:
 
   /**
    * Write a string to an output stream.
-   * If we print a space, we may pop a space from blankqueue.
+   * If we print a space, we may pop a space from blankqueue
+   * immediately, otherwise it should be printed afterwards.
    *
    * @param str the string to write, escaping characters
    * @param output the stream to write in
+   * @return how many blanks to pop and print after printing lu
    */
-  void writeEscapedPopBlanks(wstring const &str, FILE *output);
+  size_t writeEscapedPopBlanks(wstring const &str, FILE *output);
 
   /**
    * Write a string to an output stream, escaping all escapable characters

tests/data/gardenpath-mwe.dix
@@ -6,6 +6,8 @@
     <sdef n="inf"/>
     <sdef n="pr"/>
     <sdef n="prn"/>
+    <sdef n="np"/>
+    <sdef n="sent"/>
   </sdefs>
   <pardefs>
   </pardefs>
@@ -17,6 +19,15 @@
 
     <e><p><l>legge<b/>seg<b/>opp<b/>til</l> <r>legge<g><b/>seg<b/>opp<b/>til<s n="vblex"/><s n="inf"/></g></r></p></e>
     <e><p><l>seg</l> <r>seg<s n="prn"/></r></p></e>
+
+    <e><p><l>St.<b/>Petersburg</l> <r>St.<b/>Petersburg<s n="np"/></r></p></e>
+    <e><p><l>X<b/>y</l> <r>X<b/>y<s n="np"/></r></p></e>
+    <e><p><l>F</l> <r>F<s n="np"/></r></p></e>
+    <e><p><l>G</l> <r>G<s n="np"/></r></p></e>
+  </section>
+
+  <section id="final" type="inconditional">
+    <e><p><l>.</l> <r>.<s n="sent"/></r></p></e>
   </section>
 
 </dictionary>

tests/lt_proc/__init__.py
@@ -101,12 +101,28 @@ class GardenPathMwe(unittest.TestCase, ProcTest):
                         "legge opp[<br/>]",
                         "legge[][\n]L[<\/p>\n]",
                        ]
-    expectedOutputs = ["^x/*x$[ <br/> ]^opp/opp<pr>$.",
-                        "^legge/legge<vblex><inf>$ ^opp/opp<pr>$[<br/>]^x/*x$.",
+    expectedOutputs = ["^x/*x$[ <br/> ]^opp/opp<pr>$^./.<sent>$",
+                        "^legge/legge<vblex><inf>$ ^opp/opp<pr>$[<br/>]^x/*x$^./.<sent>$",
                        "^y/*y$[A]^x/*x$",
                         "[A]^y/*y$ ^x/*x$",
                         "^legge/legge<vblex><inf>$ ^opp/opp<pr>$[<br/>]",
                         "^legge/legge<vblex><inf>$[][\n]^L/*L$[<\/p>\n]",
+                       ]
+
+class GardenPathMweNewlines(unittest.TestCase, ProcTest):
+    procdix = "data/gardenpath-mwe.dix"
+    inputs          = ["""St.[
+]Petersburg[
+]X y.[][
+
+]F G.[][
+]"""]
+    expectedOutputs = ["""^St. Petersburg/St. Petersburg<np>$[
+][
+]^X y/X y<np>$^./.<sent>$[][
+
+]^F/F<np>$ ^G/G<np>$^./.<sent>$[][
+]"""
                        ]
 
 # These fail on some systems: