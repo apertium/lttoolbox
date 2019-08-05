0111e15e318a31200de454122ec5f3796e4ee38e
Amr Keleg <amr_mohamed@live.com>
2019-08-05 17:56:00 +0200

    Improve the fallback weights for unknown words
    When generating a supervised weightlist, the number of times certain tags occur
    in the corpus can be used to provide an estimate for unknown words with a matchine tag.
    Example: if <n><pl> occurred m times then
    we can assume that analyses of form ?*<n><pl> have a fallback weight that is somehow proportional to m.
    Additionally, a default weight can be estimated as further fallback weight using laplace smoothing.

--- scripts/utils/utils.py
+++ scripts/utils/utils.py
@@ -4,10 +4,38 @@
 import re
 import sys
 
+def extract_surface(tagged_line):
+	"""Extract the surface form from a tagged line
+
+	A tagged line takes the form ^surface/analysis<tag>$
+	i.e: returns surface
+	"""
+	return re.findall(r'\^.*\/', tagged_line)[0][1:-1]
+
 def extract_analysis(tagged_line):
+	"""Extract the Analysis form from a tagged line
+
+	A tagged line takes the form ^surface/analysis<tag>$
+	i.e.: returns analysis<tag>
+	"""
 	return re.sub(r'[\t ]|^(\^.*\/)|(\$)$', '', tagged_line)
 
-def generate_regex(analysis):
+def extract_tag_from_analysis(tagged_line):
+	"""Extract the tags from a tagged line
+
+	A tagged line takes the form ^surface/analysis<tag>$
+	i.e.: returns tag
+	"""
+	matches = re.findall('<.*>', extract_analysis(tagged_line))
+	if len(matches) == 0:
+		# TODO: HANDLE THE CASE NO TAGS ARE FOUND
+		# raise BaseException()
+		return None
+	return matches[0]
+
+def generate_regex(analysis, match_all_prefixes=False):
+	"""Convert an analysis form into XEROX regex"""
+
 	# Add a space after each token "REGEX concatenation"
 	analysis = ' '.join(analysis)
 
@@ -26,10 +54,15 @@ def generate_regex(analysis):
 	analysis = re.sub(r'(\<.*?\>)', lambda multichar_tag:
 		'%<{}%>'.format((re.sub(' ', '', multichar_tag.group(0)[1:-1]))), analysis)
 
-	# Surround regex with []
-	analysis = '[{}]'.format(analysis)
+	analysis = re.sub(r'[^%]([>])', '%>', analysis)
+	analysis = re.sub(r'[^%]([<])', '%<', analysis)
 
 	# TODO: Transform the regex into [?*][REGEX][?*]
 	# This may be too slow to be feasible
+	if match_all_prefixes:
+		return '[?* {}]'.format(analysis)
+
+	# Surround regex with []
+	analysis = '[{}]'.format(analysis)
 
 	return analysis
