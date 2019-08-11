afbbbdf207efc62a489bc2cd89a8a4d974627e92
Amr Keleg <amr_mohamed@live.com>
2019-08-11 13:31:26 +0200

    Make use of laplace smoothing for the constraint grammar weighting method

--- scripts/eval/constraintgrammar_fit.py
+++ scripts/eval/constraintgrammar_fit.py
@@ -31,12 +31,14 @@ if __name__ == '__main__':
 	temp_dir = tempfile.mkdtemp()
 
 	temp_weightlist = Path(temp_dir, 'temp_weightlist')
+	default_weightlist = Path(temp_dir, 'temp_default_weightlist')
 	subprocess.run(['./unannotated-corpus-to-weightlist',
-		apertium_bin, corpus, constraint_grammar, temp_weightlist])
+		apertium_bin, corpus, constraint_grammar, temp_weightlist, default_weightlist])
 
 	for input_file in sorted(os.listdir(input_directory)):
 		# Generate a bin file
 		subprocess.run(['./lt-weight',
 						apertium_bin,
 						Path(output_directory, '{}.bin'.format(input_file)),
-						temp_weightlist])
+						temp_weightlist,
+						default_weightlist])
