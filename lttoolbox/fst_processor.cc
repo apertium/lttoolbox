e5ff7b88c326c0ef2e09fc7f16976ff27f5dc17a
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2020-10-22 21:56:52 +0200

    If we saw a NUL, skip it in the input_buffer so we don't output it again
    56bbe281d8ad627898252f9e41f1a47af31fabd1 gave a little regression in
    apy making requests off by n+1

--- lttoolbox/fst_processor.cc
+++ lttoolbox/fst_processor.cc
@@ -1779,6 +1779,9 @@ FSTProcessor::analysis(FILE *input, FILE *output)
         input_buffer.setPos(last);
         input_buffer.back(1);
       }
+      if(val == 0) {
+        input_buffer.setPos(last+1);
+      }
 
       current_state = initial_state;
       lf = L"";
