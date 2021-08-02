a4b2962bc6fcc811eed1a192c826084dc04a4be2
Daniel Swanson <popcorn.tomato.dude@gmail.com>
2021-08-02 16:50:15 -0500

    missed some debug statement

--- lttoolbox/mmap.h
+++ lttoolbox/mmap.h
@@ -8,26 +8,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#include <errno.h>
-
-#include <iostream>
+//DEBUG
+//#include <iostream>
+//#include <errno.h>
 
 inline bool mmap_file(FILE* fd, void*& ptr, int& len)
 {
-  std::cerr << "mmap_file()\n";
   struct stat sb;
   if (fstat(fileno(fd), &sb) == -1) {
-    std::cerr << "fstat failed\n";
     return false;
   }
   len = sb.st_size;
-  std::cerr << "file length is " << len << "\n";
   ptr = mmap(NULL, len, PROT_READ, MAP_SHARED, fileno(fd), 0);
   if (ptr == MAP_FAILED) {
-    std::cerr << "mmap failed\nerrno = " << errno << "\n";
+    //std::cerr << "mmap failed\nerrno = " << errno << "\n";
     return false;
   }
-  std::cerr << "got pointer\n";
   return true;
 }
 
